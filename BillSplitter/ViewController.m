72ddf0e249e94aba21413c8b5185697746c52398
Alp Eren Can <alperenc@gmail.com>
2015-09-05 13:21:28 -0700

    add input accessory view to keyboard and implement done

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
 @property (weak, nonatomic) IBOutlet UILabel *splitAmountLabel;
 
+@property (strong, nonatomic) UIToolbar *accessoryView;
 @property (strong, nonatomic) NSNumberFormatter *formatter;
 
 @end
@@ -21,6 +22,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.billAmountTextField.delegate = self;
+    
+    self.accessoryView = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44.0)];
+    UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                          target:self
+                                                                          action:@selector(calculateSplitAmount)];
+    
+    [self.accessoryView setItems:@[[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                                 target:nil
+                                                                                 action:nil],
+                                   done]];
+    
+    self.billAmountTextField.inputAccessoryView = self.accessoryView;
+    
     self.splitAmountLabel.text = @"";
 }
 
@@ -29,7 +44,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)calculateSplitAmount:(id)sender {
+- (IBAction)calculateSplitAmount {
     
     self.formatter = [NSNumberFormatter new];
     
@@ -41,12 +56,15 @@
     
     self.formatter.numberStyle = NSNumberFormatterCurrencyStyle;
     
-    self.splitAmountLabel.text = [self.formatter stringFromNumber:splitAmount];
+    self.splitAmountLabel.text = [NSString stringWithFormat:@"Everybody pays: %@", [self.formatter stringFromNumber:splitAmount]];
+    
+    [self.billAmountTextField resignFirstResponder];
     
 }
 
 - (IBAction)setSliderValue:(UISlider *)sender {
     [sender setValue:(ceilf(sender.value)) animated:YES];
+    [self calculateSplitAmount];
 }
 
 @end
